49a79d4cd109f52b618d53291676907cb238cc74
mkrajnak <mkrajnak>
2019-10-19 23:03:33 +0200

    Add project files

---
+++ Step0/neuron.cpp
@@ -0,0 +1,22 @@
+/*
+ * Architektury výpočetních systémů (AVS 2019)
+ * Projekt c. 1 (ANN)
+ * Login: xlogin00
+ */
+
+#include <cstdlib>
+#include "neuron.h"
+
+float evalNeuron(
+  size_t inputSize,
+  size_t neuronCount,
+  const float* input,
+  const float* weights,
+  float bias,
+  size_t neuronId
+)
+{
+  //TODO: Step0 - Fill in the implementation, all the required arguments are passed.
+  //              If you don't use them all you are doing something wrong!
+  return 0.0f;
+}
